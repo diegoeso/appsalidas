9dd6d14b528d4bf5adbeeaaff26a9b57b6fcf7b8
CamiloEscobar98 <camilo_escobar2398@outlook.com>
2020-08-03 22:12:12 -0500

    1.2.2 Migrations and Model, Fix Participants

---
+++ database/migrations/2020_08_04_030438_create_participants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable(false)->comment('User id');
+            $table->bigInteger('activity_id')->unsigned()->nullable(false)->comment('Activity id');
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
