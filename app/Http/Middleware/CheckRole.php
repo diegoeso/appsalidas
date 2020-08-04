9f1cb095163e656e194e0c9756a159cb5568ce44
CamiloEscobar98 <camilo_escobar2398@outlook.com>
2020-08-04 12:00:14 -0500

    1.3.8 Fix Kernel and import Middleware CheckRole

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        if (Auth::user()->hasRole($role)) {
+            return  Auth::user()->hasRoleAvailable($role) ?  back() :  $next($request);
+        }
+        return back();
+    }
+}
